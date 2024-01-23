be1e8ad5aab111a462a2474c772b99fd46082e79
michael <michaelutoh21@gmail.com>
2024-01-23 09:24:50 +0100

    Refactor cors options

--- frontend/src/components/portfolioCard.jsx
+++ frontend/src/components/portfolioCard.jsx
@@ -12,7 +12,7 @@ const PortfolioCard = () => {
     };
 
     const handlePortfolioData = async () => {
-        await axios.get(`https://at-africa-backend-5hfnpu09x-michaelutoh.vercel.app/portfolios`)
+        await axios.get('https://at-africa-backend-5hfnpu09x-michaelutoh.vercel.app/portfolios')
             .then((res) => {
                 console.log(res);
                 setPortfolioData(res.data.portfolios);
